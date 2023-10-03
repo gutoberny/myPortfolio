7ee60d3ab9f002a5f84a967547c885771c8fca35
Gustavo Berny <pelotas.berny93@gmail.com>
2023-10-02 21:47:15 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Button } from "./components/ui/button";
+import { Github } from "lucide-react";
+
 export function App() {
   return (
     <>
-      <h1>Uuup!</h1>
+      <div>
+        <div className="px-6 py-3 flex items-center justify-between border-b">
+          <h1 className="text-xl font-bold">upload.ai</h1>
+
+          <div className="flex items-center gap-3">
+            <span className="text-sm text-muted-foreground">
+              Desenvolvido com ❤️ no NLW da Rocketseat
+            </span>
+            <Button variant="outline">
+              <Github className="w-4 h-4 mr-2" />
+              Github
+            </Button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
